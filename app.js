946a63c8995ed372189b3cc3d95ed2f6b516ab3e
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-09-10 03:54:34 -0700

    REMAIN

---
+++ app.js
@@ -0,0 +1,126 @@
+// var firstName = "zohaib"
+
+// if(firstName === "danihs"){
+//     alert("worng ")
+// }else{
+//     alert("true")
+// }
+
+//if ture py chaly gii or else false main
+
+// var num = 5;
+// if (num === "5") {
+//   alert(num);
+// } else {
+//   alert(false);
+// }
+
+// triple equal sign type and value dono cheeck karta hai 
+// double  equal sign value dono cheeck karta hai 
+
+
+// PRACTICE 1
+// var a = prompt('ENTER YOUR CITY')
+// if(a==='karachi'){
+//     alert('Welcome to city of lights')
+// }
+
+// PRACTICE 2
+// var a = prompt('ENTER YOUR GENDER')
+// if(a==='male'){
+//     alert(' Good Morning Sir.')
+// }
+// if(a==='female'){
+//     alert(' Good Morning Madam.')
+
+// }
+
+
+// PRACTICE 3
+
+// var a = prompt('ENTER TRAFFIC SIGNAL COLOR')
+// if(a==='red'){
+//     alert('Must Stop')
+// }
+// if(a==='yellow'){
+//     alert('Ready to move')
+// }
+// if(a==='green'){
+//     alert('Move now')
+// }
+
+// PRATICE 4
+// var a = prompt('ENTER YOUR REMAINING FUEL')
+// if(a <'0.25'){
+// alert('Please refill the fuel in your car”')
+// }
+
+
+
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+//     alert("condition 2 is true");
+//     }
+//     if (++c < 14){
+//     alert("condition 3 is true");
+//     }
+//     if(c === 14){
+//     alert("condition 4 is true");
+//     }
+//      var materialCost = 20000;
+//     var laborCost = 2000;
+//     var totalCost = materialCost + laborCost;
+//     if (totalCost === laborCost + materialCost){
+//     alert("The cost equals");
+//     }
+//     if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+//  if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// game
+
+var a = +prompt('ENETER YOUR 1 SUBJECT MARK')
+var b = +prompt('ENETER YOUR 2 SUBJECT MARK')
+var c = +prompt('ENETER YOUR 3 SUBJECT MARK')
+var d = a+b+c
+    if (d < 190) {
+document.write('j')
+    } else if (d < 20) {
+        greeting = "Good day";
+      } else {
+        greeting = "Good evening";
+      }
+
+
+
+
+
+// document.write('TOTAL MARK 300'+'<br>'+'MARKS OBTAINED'+d+'<br>'+'YOUR PERCSENTAGE'+(300/d)*100+'<br>'+'GRADE B'+'<br>'+'REMARK:YOU NEED TO IMPROVE')
+
+
+
+
+
+
+
+
+
+
